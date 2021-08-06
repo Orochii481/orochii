424308fa85619c31c12f2543da75b30ae8fe108d
Orochii481 <83424384+Orochii481@users.noreply.github.com>
2021-08-06 15:32:37 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,130 @@
+const help = (pushname, prefix) => { 
+
+	return `
+
+──────❶*Bills BOT*❷───────
+(っ◕‿◕)っ Olá ${pushname}
+Bem Vindo ao Bot da WOLF
+Lider:×᷼×🔥𝐀𝐋𝐅𝐀×᷼×𝐋𝐔𝐏𝐈𝐍×᷼×
+┌─────────❶.❷────────
+║
+╠══〘 MENU 〙══
+║
+║🌕*${prefix}figu*
+║🌕*${prefix}sorteio*
+║🌕*${prefix}tts*
+║🌕*${prefix}gostosa*
+║🌕*${prefix}nicks*
+║🌕*${prefix}attp*
+║🌕*${prefix}gays*
+║🌕*${prefix}pau*
+║🌕*${prefix}abraço*
+║🌕*${prefix}marcar*
+║🌕*${prefix}add [55]*
+║🌕*${prefix}banir [@]*
+║🌕*${prefix}promover [@]*
+║🌕*${prefix}rebaixar*
+║🌕*${prefix}admins*
+║🌕*${prefix}marcar2*
+║🌕*${prefix}bc [texto]* (ele faz uma ™)
+║🌕*${prefix}marcar3*
+║🌕*${prefix}bloqueados*
+║🌕*${prefix}bloquear [@]*
+║🌕*${prefix}desbloquear [@]*
+║🌕*${prefix}casal*
+║🌕*${prefix}amor*
+║🌕*${prefix}bemvindo [1/0]*
+║🌕*${prefix}clonar [@]*
+║🌕*${prefix}tts [texto]*
+║🌕*${prefix}setnome*
+║🌕*${prefix}roleta*
+║🌕*${prefix}setfoto*
+║🌕*${prefix}infogp*
+║🌕*${prefix}gerarnick*
+║🌕*${prefix}bomdia*
+║🌕*${prefix}boanoite*
+║🌕*${prefix}punheteiros*
+║
+╠══〘 GRUPO 〙══
+║
+║🌕*${prefix}banir*
+║🌕*${prefix}leveling [on/off]*
+║🌕*${prefix}level*
+║🌕*${prefix}add*
+║🌕*${prefix}promover*
+║🌕*${prefix}setfoto [na legenda]*
+║🌕*${prefix}setname [texto]*
+║🌕*${prefix}rebaixar*
+║🌕*${prefix}admins*
+║🌕*${prefix}marcar*
+║🌕*${prefix}marcar2*
+║🌕*${prefix}marcar3*
+║🌕*${prefix}bemvindo [1/0]*
+║🌕*${prefix}grupoinfo*
+║🌕*${prefix}bomdia*
+║🌕*${prefix}boatarde*
+║🌕*${prefix}boanoite*
+║🌕*${prefix}antifake*
+║
+╠══〘 ESPECIFICO DO BOT 〙══
+║
+║🌕*${prefix}clonar [@]*
+║🌕*${prefix}termux*
+║🌕*${prefix}gay [@]*
+║🌕*${prefix}wame*
+║🌕*${prefix}map (nome)*
+║🌕*${prefix}setppbot (marque uma img)*
+║🌕*${prefix}pinterest (nome)*
+║🌕*${prefix}desligar (so para o dono)*
+║🌕*${prefix}timer*
+║
+╠══〘 MAIS ALGUNS 〙══
+║
+║🌕 *${prefix}ttp [texto]*
+║🌕 *${prefix}testime*
+║🌕 *${prefix}tomp3*
+║🌕 *${prefix}ytsearch*
+║🌕 *${prefix}moddroid [jogo/app]*
+║🌕 *${prefix}nomegp*
+║🌕 *${prefix}darkjokes (memes aleatórios)*
+║🌕 *${prefix}animecry*
+║🌕 *${prefix}gay1*
+║🌕 *${prefix}alerta*
+║🌕 *${prefix}analise*
+║
+╠══〘 Premium 〙══
+║
+║👑 *em produção*
+║
+╠══〘 OUTROS /2 〙══
+║
+║🌕*${prefix}antilink [1/0]*
+║🌕*${prefix}brainly [pergunta]*
+║🌕*${prefix}setnomebot*
+║🌕*${prefix}meme*
+║
+╠══〘 INTERATIVOS 〙══
+║
+╠══NOTA »
+║Mandar a msg sem o prefixo
+╠════════════════════
+║
+║🌕 *bah*
+║🌕 *oii*
+║🌕 *yamete*
+║🌕 *canta ai bot*
+║🌕 *grita*
+║🌕 *musica*
+║🌕 *fds*
+║🌕 *Sexo*
+║
+╠══〘 Bills Bot 〙══
+║
+║ *NOME: 𝐖𝐎𝐋𝐅 || ✞ঔৣ۝𝔒𝔯𝔬𝔠𝔥𝔦𝔦۝ঔৣ✞,*
+║ *WPP 𝐖𝐎𝐋𝐅 || ✞ঔৣ۝𝔒𝔯𝔬𝔠𝔥𝔦𝔦۝ঔৣ✞: wa.me/+5511952761206*
+║
+║
+║
+╚═〘 Bills 〙`
+}
+exports.help = help
---
+++ src/help1.js
@@ -0,0 +1,41 @@
+const help1 = (prefix) => {
+
+	return `
+┣━━━━°❀ ❬ *CRIAR* ❭ ❀°━━━━┓
+┃
+┣⊱❥ *cross-fire*
+┣⊱❥ *gtav*
+┣⊱❥ *logotop*
+┣⊱❥ *coffee-cup2*
+┣⊱❥ *shadow*
+┣⊱❥ *happy-birthday*
+┣⊱❥ *cemitery*
+┣⊱❥ *harry-potter*
+┣⊱❥ *wood-black*
+┣⊱❥ *chrome*
+┣⊱❥ *flaming*
+┣⊱❥ *puff*
+┣⊱❥ *dado3d*
+┣⊱❥ *vintage*
+┣⊱❥ *glowing3d*
+┣⊱❥ *camouflage*
+┣⊱❥ *rainbow-shine*
+┣⊱❥ *honey*
+┣⊱❥ *wolf-metal*
+┣⊱❥ *romantic*
+┣⊱❥ *cover*
+┣⊱❥ *smoke*
+┣⊱❥ *burn-papper*
+┣⊱❥ *naruto*
+┣⊱❥ *lovemsg*
+┣⊱❥ *megrass*
+┣⊱❥ *coffee-cup*
+┣⊱❥ *love-text*
+┣⊱❥ *butterfly*
+
+════════════════════
+*Bills* 🤗
+════════════════════`
+}
+exports.help1 = help1
+
